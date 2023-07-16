9f6e5a5e67c083ef76c4940b0e663f2f6f3fc06f
etachott <bfwaend@gmail.com>
2023-07-15 22:22:03 -0300

    feat: add simple vector implementation

--- includes/vector.hpp
+++ includes/vector.hpp
@@ -6,7 +6,7 @@
 /*   By: edu <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/15 02:42:15 by edu               #+#    #+#             */
-/*   Updated: 2023/07/15 02:52:16 by edu              ###   ########.fr       */
+/*   Updated: 2023/07/15 22:10:37 by etachott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,16 @@
 #define VECTOR_HPP
 
 #include <string>
+
+namespace ft {
+
+template <typename T>
+class vector {
+	public:
+		vector();
+		~vector();
+};
+
+}
 
 #endif
