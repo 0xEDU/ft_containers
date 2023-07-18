d238c979973ca17e4940528c92567531290d11d4
etachott <bfwaend@gmail.com>
2023-07-18 19:00:56 -0300

    feat(tests): add simple assert function

--- tests/testUtils.hpp
+++ tests/testUtils.hpp
@@ -6,7 +6,7 @@
 /*   By: etachott <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/18 18:37:17 by etachott          #+#    #+#             */
-/*   Updated: 2023/07/18 18:38:56 by etachott         ###   ########.fr       */
+/*   Updated: 2023/07/18 19:00:40 by etachott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,11 @@
 #define END "\033[0m"
 
 template <typename T>
-void assert(T &lhs, T &rhs, const char *testName) {
-	if (lhs != rhs) {
-		std::cout << "[" << RED << "KO" << END << "] " << testName << std::endl;
-	} else {
+void assert(const T &lhs, const T &rhs, const char *testName) {
+	if (lhs == rhs) {
 		std::cout << "[" << GRN << "OK" << END << "] " << testName << std::endl;
+	} else {
+		std::cout << "[" << RED << "KO" << END << "] " << testName << std::endl;
 	}
 	return ;
 }
