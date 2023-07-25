7d95ea24e6a037236005310a099ca45285585d7c
etachott <bfwaend@gmail.com>
2023-07-25 18:57:55 -0300

    feat(tests/iterator): add test for '!=' operator and break tests into functions

--- tests/vector_iterator/vector_iterator_tests.cpp
+++ tests/vector_iterator/vector_iterator_tests.cpp
@@ -14,11 +14,27 @@
 #include "vector_iterator.hpp"
 #include "test_utils.hpp"
 
-int main(void) {
-	ft::vector_iterator<int> i1;
-	ft::vector_iterator<int> i2 = i1;
-	ft::vector_iterator<int> i3(i2);
+void test_equivalence() {
+    ft::vector_iterator<int> i1;
+    ft::vector_iterator<int> i2 = i1;
 
-	assert(i1 == i2, "Equivalence comparison");
+    assert(i1 == i2, "Equivalence comparison");
+}
+
+void test_difference() {
+    int i[5] = {1, 1, 1, 1, 1};
+    int a[5] = {2, 2, 2, 2, 2};
+    int *p1 = i;
+    int *p2 = a;
+
+    ft::vector_iterator<int *> i1(p1);
+    ft::vector_iterator<int *> i2(p2);
+
+    assert(i1 != i2, "Difference comparison");
+}
+
+int main() {
+    test_equivalence();
+    test_difference();
 	return (0);
 }
