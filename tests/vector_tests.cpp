022f49929e632f159fcb1817a7b26561f09bd370
etachott <bfwaend@gmail.com>
2023-11-02 18:59:07 -0300

    tests(Vector): add test for vector initialization

--- tests/vector_tests.cpp
+++ tests/vector_tests.cpp
@@ -2,10 +2,20 @@
 #include "test_utils.hpp"
 #include <cstddef>
 
+// TEST_F(vectorBegin) {
+//   ft::vector<int> v;
+//   ft::vector<int>::iterator it = v.begin();
+//
+// }
+
 TEST_F(vectorInstantiation) {
   ft::vector<int> v;
+
+  assert(v.empty() == true, "Vector is empty");
+  assert(v.size() == 0, "Vector has no elements");
 }
 
 TEST_SUITE {
     RUN_TEST(vectorInstantiation);
+    // RUN_TEST(vectorBegin);
 }
