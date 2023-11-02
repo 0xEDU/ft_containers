5fecc38355b49994d152ed7321c54c1e3b9b29ed
etachott <bfwaend@gmail.com>
2023-11-02 20:28:55 -0300

    feat(Vector): add test for vector instantiation with custom allocator

--- tests/vector_tests.cpp
+++ tests/vector_tests.cpp
@@ -16,14 +16,28 @@ TEST_F(vectorInstantiation) {
 }
 
 TEST_F(vectorInstantiationWithSize) {
-  ft::vector<int> v(5);
+  ft::vector<int> v1(5);
+
+  assert(v1.empty() == true, "Vector is empty");
+  assert(v1.size() == 5, "Vector has size 5");
+
+  ft::vector<int> v2(10);
+
+  assert(v2.empty() == true, "Vector is empty");
+  assert(v2.size() == 10, "Vector has size 10");
+}
+
+TEST_F(vectorInstantiationWithCustomAllocator) {
+  std::allocator<int> alloc;
+  ft::vector<int> v(alloc);
 
   assert(v.empty() == true, "Vector is empty");
-  assert(v.size() == 5, "Vector has size 5");
+  assert(v.size() == 0, "Vector has no size");
 }
 
 TEST_SUITE {
-    RUN_TEST(vectorInstantiation);
-    RUN_TEST(vectorInstantiationWithSize);
-    // RUN_TEST(vectorBegin);
+  RUN_TEST(vectorInstantiation);
+  RUN_TEST(vectorInstantiationWithSize);
+  RUN_TEST(vectorInstantiationWithCustomAllocator);
+  // RUN_TEST(vectorBegin);
 }
