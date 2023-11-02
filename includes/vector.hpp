468205b1cc6a35d3ebf688658fe79abae20ea4da
etachott <bfwaend@gmail.com>
2023-11-02 20:29:28 -0300

    feat(Vector): add constructor receiving a custom allocator

--- includes/vector.hpp
+++ includes/vector.hpp
@@ -17,30 +17,36 @@ public:
   typedef typename Allocator::pointer pointer;
   typedef const typename Allocator::const_pointer const_pointer;
   typedef vector_iterator<T *> iterator;
-  typedef vector_iterator<const T *> const_iterator;
+  // typedef vector_iterator<const T *> const_iterator;
   // reverse_iterator
   // const_reverse_iterator
 
 public:
-  vector() : _ptr(NULL), _allocator(), _size(0), _begin(NULL), _end(NULL) {};
-  vector(size_type size) : _ptr(NULL), _allocator(), _size(size), _begin(NULL), _end(NULL) {
+  // Constructors
+  vector() : _ptr(NULL), _allocator(), _size(0), _begin(NULL), _end(NULL){};
+
+  explicit vector(size_type size)
+      : _ptr(NULL), _allocator(), _size(size), _begin(NULL), _end(NULL) {
     this->_ptr = this->_allocator.allocate(size);
     this->_begin = this->_ptr;
     this->_end = this->_ptr;
   };
+
+  explicit vector(const Allocator &alloc)
+      : _ptr(NULL), _allocator(alloc), _size(0), _begin(NULL), _end(NULL) {}
+
+  // Destructors
   ~vector() {
     if (this->_ptr != NULL)
       this->_allocator.deallocate(this->_ptr, this->_size);
   };
 
-  size_t size() {
-    return this->_size;
-  }
+  // Capacity
+  size_t size() { return this->_size; }
 
-  bool empty() {
-    return this->_begin == this->_end;
-  }
+  bool empty() { return this->_begin == this->_end; }
 
+  // Iterators
   // iterator begin() {
   //   return typename ft::vector<T>::iterator(_ptr);
   // };
