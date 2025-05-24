46a0a0b2845b41a0cd449a361b810837d686eb35
Eduardo Tachotte <bfwaend@gmail.com>
2025-05-24 16:12:38 -0300

    (refactor): format test_utils

--- tests/test_utils.hpp
+++ tests/test_utils.hpp
@@ -10,30 +10,31 @@
 #define LOG(x) std::cout << x << std::endl;
 
 #define TEST_F(functionName) void functionName##_test(void)
-#define RUN_TEST(testName) { \
-	std::cout << "==== " << #testName << " ====" << std::endl; \
-	testName##_test(); \
-	std::cout << std::endl; \
-}
+#define RUN_TEST(testName)                                                     \
+  {                                                                            \
+    std::cout << "==== " << #testName << " ====" << std::endl;                 \
+    testName##_test();                                                         \
+    std::cout << std::endl;                                                    \
+  }
 #define TEST_SUITE int main()
 
 template <typename T, typename U>
 void assert(const T &lhs, const U &rhs, const char *testName) {
-	if (lhs == rhs) {
-		std::cout << "[" << GRN << "OK" << END << "] " << testName << std::endl;
-	} else {
-		std::cout << "[" << RED << "KO" << END << "] " << testName << std::endl;
-	}
-	return ;
+  if (lhs == rhs) {
+    std::cout << "[" << GRN << "OK" << END << "] " << testName << std::endl;
+  } else {
+    std::cout << "[" << RED << "KO" << END << "] " << testName << std::endl;
+  }
+  return;
 }
 
 void assert(bool assertion, const char *testName) {
-	if (assertion == true) {
-		std::cout << "[" << GRN << "OK" << END << "] " << testName << std::endl;
-	} else {
-		std::cout << "[" << RED << "KO" << END << "] " << testName << std::endl;
-	}
-	return ;
+  if (assertion == true) {
+    std::cout << "[" << GRN << "OK" << END << "] " << testName << std::endl;
+  } else {
+    std::cout << "[" << RED << "KO" << END << "] " << testName << std::endl;
+  }
+  return;
 }
 
 #endif
